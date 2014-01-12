7c30a5283bd4ad8672194d3212c72de5995a109f
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2014-01-12 14:13:15 +0000

    take 47

---
+++ node/SimpleDomain.js
@@ -0,0 +1,36 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4,
+maxerr: 50, node: true */
+/*global */
+
+(function () {
+    "use strict";
+    
+    var os = require("os");
+        //figlet= require("/node_modules/figlet");
+        
+    
+    function cmdGetMemory() {
+        return {total: os.totalmem(), free: os.freemem()};
+    }
+    
+    
+    function init(DomainManager) {
+        if (!DomainManager.hasDomain("simple")) {
+            DomainManager.registerDomain("simple", {major: 0, minor: 1});
+        }
+        DomainManager.registerCommand(
+            "simple",       // domain name
+            "getMemory",    // command name
+            cmdGetMemory,   // command handler function
+            false,          // this command is synchronous
+            "Returns the total and free memory on the user's system in bytes",
+            [],             // no parameters
+            [{name: "memory",
+                type: "{total: number, free: number}",
+                description: "amount of total and free memory in bytes"}]
+        );
+    }
+    
+    exports.init = init;
+    
+}());
